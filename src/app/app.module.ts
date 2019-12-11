35c8ad18c0936f30d06ae2170b2e5a4100430839
Lucas Campos <lucas.campos@simply.com.br>
2019-12-11 17:17:33 -0300

    Atualização do angular para a versão 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,13 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NotFoundComponent } from './not-found/not-found.component'
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { LocationStrategy, HashLocationStrategy, registerLocaleData } from '@angular/common';
 import { LoginComponent } from './security/login/login.component';
 import { UserDetailComponent } from './header/user-detail/user-detail.component';
 import { ApplicationErrorHandler } from './app.error-handler';
+import locatePt from '@angular/common/locales/pt'
 
+registerLocaleData(locatePt, 'pt')
 
 @NgModule({
   declarations: [
@@ -52,7 +54,7 @@ import { ApplicationErrorHandler } from './app.error-handler';
   ],
   providers: [
     {provide: LocationStrategy, useClass: HashLocationStrategy},
-    {provide: LOCALE_ID, useValue: 'pt-BR'},
+    {provide: LOCALE_ID, useValue: 'pt'},
     {provide: ErrorHandler, useClass: ApplicationErrorHandler}
   ],
   bootstrap: [AppComponent]
